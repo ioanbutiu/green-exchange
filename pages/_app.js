17581530b47ed7f59c799882f3ca0b5a32e796b2
ioan butiu <36743419+ioanbutiu@users.noreply.github.com>
2022-09-20 12:09:12 -0400

    ga tag

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,28 @@ import Head from 'next/head';
 import Layout from '../components/Layout';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/custom.scss';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as ga from '../lib/ga';
 
 function App({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			ga.pageview(url);
+		};
+		//When the component is mounted, subscribe to router changes
+		//and log those page views
+		router.events.on('routeChangeComplete', handleRouteChange);
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
 	return (
 		<Layout>
 			<Head>
