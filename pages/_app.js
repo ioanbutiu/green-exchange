d65e6b0aaa1976a4bf7806c555a05be62ea05911
ioan butiu <36743419+ioanbutiu@users.noreply.github.com>
2022-09-20 16:56:35 -0400

    fathom tracking

--- pages/_app.js
+++ pages/_app.js
@@ -5,26 +5,50 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/custom.scss';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 
 import * as ga from '../lib/ga';
 
 function App({ Component, pageProps }) {
 	const router = useRouter();
+
 	useEffect(() => {
-		const handleRouteChange = (url) => {
-			ga.pageview(url);
-		};
-		router.events.on('routeChangeComplete', handleRouteChange);
-		router.events.on('hashChangeComplete', handleRouteChange);
+		// Initialize Fathom when the app loads
+		// Example: yourdomain.com
+		//  - Do not include https://
+		//  - This must be an exact match of your domain.
+		//  - If you're using www. for your domain, make sure you include that here.
+		Fathom.load('MMRXHNOJ', {
+			includedDomains: ['thegix.com', 'greenexchange.us'],
+		});
+
+		function onRouteChangeComplete() {
+			Fathom.trackPageview();
+		}
+		// Record a pageview when route changes
+		router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+		// Unassign event listener
 		return () => {
-			router.events.off('routeChangeComplete', handleRouteChange);
-			router.events.off('hashChangeComplete', handleRouteChange);
+			router.events.off('routeChangeComplete', onRouteChangeComplete);
 		};
-	}, [router.events]);
+	}, []);
+
+	// useEffect(() => {
+	// 	const handleRouteChange = (url) => {
+	// 		ga.pageview(url);
+	// 	};
+	// 	router.events.on('routeChangeComplete', handleRouteChange);
+	// 	router.events.on('hashChangeComplete', handleRouteChange);
+	// 	return () => {
+	// 		router.events.off('routeChangeComplete', handleRouteChange);
+	// 		router.events.off('hashChangeComplete', handleRouteChange);
+	// 	};
+	// }, [router.events]);
 	return (
 		<Layout>
 			{/* Global Site Tag (gtag.js) - Google Analytics */}
-			<Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${ga.GA_TRACKING_ID}`} />
+			{/* <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${ga.GA_TRACKING_ID}`} />
 			<Script
 				id="gtag-init"
 				strategy="afterInteractive"
@@ -38,7 +62,7 @@ function App({ Component, pageProps }) {
             });
           `,
 				}}
-			/>
+			/> */}
 
 			{/* <Script
 				strategy="lazyOnload"
